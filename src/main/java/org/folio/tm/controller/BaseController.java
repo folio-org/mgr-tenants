2d0f88c9bb3fd734cb57d1ddaf487ef431d6d92a
Pavel Filippov <pavel_filippov@epam.com>
2024-02-21 12:20:06 +0200

    MGRAPPS-8 Implement router prefix for the generated endpoints

---
+++ src/main/java/org/folio/tm/controller/BaseController.java
@@ -0,0 +1,6 @@
+package org.folio.tm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(path = "${application.router.path-prefix}")
+public class BaseController {}
