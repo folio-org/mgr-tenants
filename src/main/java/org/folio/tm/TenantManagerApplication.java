b425e01fdac60c17509afe14d1c0b67de876a6cd
okuzminov <oleksii_kuzminov@epam.com>
2024-05-20 14:05:13 +0200

    MGRTENANT-18: Secure mgr-tenants HTTP endpoints with SSL

--- src/main/java/org/folio/tm/TenantManagerApplication.java
+++ src/main/java/org/folio/tm/TenantManagerApplication.java
@@ -1,7 +1,5 @@
 package org.folio.tm;
 
-import java.security.Security;
-import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,9 +12,6 @@ public class TenantManagerApplication {
    * @param args command line arguments.
    */
   public static void main(String[] args) {
-    if (Security.getProvider("BCFIPS") == null) {
-      Security.addProvider(new BouncyCastleFipsProvider());
-    }
     SpringApplication.run(TenantManagerApplication.class, args);
   }
 }
