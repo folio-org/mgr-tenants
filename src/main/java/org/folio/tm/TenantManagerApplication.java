3ddd29d0677d46742408216537144d79374dcaa3
Taras_Spashchenko <Taras_Spashchenko@epam.com>
2024-05-29 16:12:24 +0200

    Add FIPS compliance check and lost Kong configuration parameters

--- src/main/java/org/folio/tm/TenantManagerApplication.java
+++ src/main/java/org/folio/tm/TenantManagerApplication.java
@@ -1,8 +1,12 @@
 package org.folio.tm;
 
+import static org.folio.common.utils.tls.FipsChecker.getFipsChecksResultString;
+
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log4j2
 @SpringBootApplication
 public class TenantManagerApplication {
 
@@ -12,6 +16,7 @@ public class TenantManagerApplication {
    * @param args command line arguments.
    */
   public static void main(String[] args) {
+    log.info(getFipsChecksResultString());
     SpringApplication.run(TenantManagerApplication.class, args);
   }
 }
